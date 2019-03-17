79997158b562c6ab54a4b4772b2310b950c095ff
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-03-16 20:20:53 -0700

    errors fixed, added buttons and css

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let marker, tracker
+let marker
+let map
+let tracker
   
   // Initialize and add the map
   function initMap() {
@@ -8,27 +10,39 @@ let marker, tracker
     map = new google.maps.Map(document.getElementById('map'), { zoom: 4, center: uluru });
     // The marker, positioned at Uluru
     marker = new google.maps.Marker({ position: uluru, map: map });
+    createButton('START', startTracking, "btn btn-success")
+    createButton('STOP', stopTracking, "btn btn-danger")
+}
+
+function createButton (text, clickFunction, classes) {
+    let button = document.createElement("button")
+    button.addEventListener('click', clickFunction)
+    button.innerHTML = text
+    button.className = classes
+    
+    document.querySelector(".buttons").append(button)
+    console.log(button)
 }
 
 const trackMovement = () => {
     // Fetch coordinates of ISS
     fetch('http://api.open-notify.org/iss-now.json')
     .then(r => r.json())
-    .then(response => {
+    .then(r => {
         // Declare variables latitude and longitude that are both properties of response.iss_position
-        const {latitude, longitude} = response.iss_position
+        const {latitude, longitude} = r.iss_position
         // Changed latitude and longitude from string to numbers
         marker.setPosition({
             lat: parseInt(latitude), 
-            lng: parseInt(longitude),
+            lng: parseInt(longitude)
         })
         map.panTo({
             lat: parseInt(latitude), 
-            lng: parseInt(longitude),
+            lng: parseInt(longitude)
         })
 
     })
-    .catch(err => console.error(err))
+    .catch(e => console.error(e))
 }
 
 // We don't want the interval running all the time so we wrap it in a function
